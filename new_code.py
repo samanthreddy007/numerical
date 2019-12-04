f5bc9fdc5aa0af597f228c145c81b760d905e249
Samanth Reddy Karra <reddysamanth007@gmail.com>
2019-12-04 17:16:59 -0500

    Update new_code.py

--- new_code.py
+++ new_code.py
@@ -1,5 +1,7 @@
 
 
+
+
 # -*- coding: utf-8 -*-
 """
 Created on Mon Dec  2 19:32:05 2019
@@ -13,18 +15,22 @@ from numpy import linalg as LA
 import timeit
 import matplotlib.pyplot as plt
 from numpy import random
-
-
+import math
+#Your statements here
 
 class Algorithms():
     
     def __init__(self,matrixsize):
-        self.matrixsize=matrixsize
-        self.x_initial=[1]*matrixsize
-        self.b = random.rand(matrixsize)       
-        self.p = random.rand(matrixsize,matrixsize)    
-        self.A=np.add(0.5 *np.add(self.p,self.p.transpose()), matrixsize*np.identity(matrixsize))
-    
+        while (1):
+            self.matrixsize=matrixsize
+            self.x_initial=[1]*matrixsize
+            self.b = random.rand(matrixsize)       
+            self.p = random.rand(matrixsize,matrixsize)    
+            self.A=np.add(0.5 *np.add(self.p,self.p.transpose()), matrixsize*np.identity(matrixsize))
+            x=solve(self.A,self.b)
+            if all([y>0 for y in x]):
+                break
+            
     def gauss(self,n,max_error):
         L = np.tril(self.A)
         U = self.A - L
@@ -98,8 +104,8 @@ class Algorithms():
         print('Time: ', stop - start)
         return x,error_list,i,spectral_radius,stop - start 
 
-
-    def SOR(self,n,max_error,w):
+        
+    def SOR(self,n,max_error,w=None):
         x=self.x_initial
         D_L = np.tril(self.A)
         D = np.diag(np.diag(self.A))
@@ -111,11 +117,23 @@ class Algorithms():
         error=float('inf')
        # D = np.diag(np.diag(A))
         #R = A -D
-    
+        def opt_w(D_,L_,U_):
+            w_=0
+            Tj = np.linalg.solve(-(D_+(w_*L_)),((1-w_)*L_)+U_)
+            eigsTj, _ = np.linalg.eig(Tj)
+            
+            spectral_radiusj = np.max(np.abs(eigsTj))
+            w_=2/(1+np.sqrt(1-(spectral_radiusj**2)))
+            return w_
+        optimal_w=opt_w(D,L,U)
+        if w==None:    
+            w=optimal_w
+            
         T = np.linalg.solve(-(D+(w*L)),((1-w)*L)+U)
         eigsT, _ = np.linalg.eig(T)
         spectral_radius = np.max(np.abs(eigsT))
        
+    
         start = timeit.default_timer()
      
         while i<n  :
@@ -135,6 +153,96 @@ class Algorithms():
         print('Time: ', stop - start)
         return x,error_list,i,spectral_radius,stop - start 
 
+
+    def TriangleAlgo(self,max_error):
+            print('Running Triangle Algorithm.......')
+            vectors = []
+            flag = True
+            p = np.zeros(len(self.A))
+            min_norm = np.linalg.norm(self.b)
+            max_norm = 0
+            min_index = -1
+            p1 = -self.b
+            alphas = np.zeros(len(self.A)+1)
+            found_seed = False
+    
+            
+            ''' Find the vertices of convex '''
+            
+            for index,col in enumerate(self.A.transpose()):
+                vectors.append(col)
+            
+                if(np.linalg.norm(col)< min_norm):
+                    p1 = col
+                    min_norm = np.linalg.norm(col)
+                    min_index = index
+                if(np.linalg.norm(col)> max_norm):
+                    max_norm = np.linalg.norm(col)  
+            vectors.append(-self.b)
+            vectors = np.array(vectors)
+            alphas[min_index] = 1
+            iteration = 0
+            start = timeit.default_timer()
+            while(1):
+                
+                iteration = iteration + 1
+                
+                
+                
+                ''' Find Pivot '''
+                for index, vector in enumerate(vectors):
+                    if(np.linalg.norm(p1 - vector) > np.linalg.norm(vector)):
+                        vj = vector
+                        j = index
+                        break
+        
+            
+                ''' Find P'' '''
+                if((np.dot(p-p1, vj-p1)/np.linalg.norm(vj-p1)) > np.linalg.norm(vj-p1)):
+                    print('entered')
+                    p1 = vj
+                    alphas = np.zeros(len(self.A)+1)
+                    alphas[j] = 1
+                    flag = True
+                    
+                else:
+                
+                    alpha = np.dot(p-p1, vj-p1)/math.pow(np.linalg.norm(vj-p1),2)
+                    temp = alphas[j]
+                    alphas = (1 - alpha) * alphas
+                    alphas[j] = (1 - alpha) * temp + alpha
+                    '''
+                    new_p = np.zeros(len(alphas)-1)
+                
+                    for i in range(len(alphas)):
+                        
+                        new_p = new_p + alphas[i] * vectors[i]'''
+                    p1 = (1 - alpha)* p1 + alpha * vj
+                    #p1 = new_p
+                    flag = False
+                    
+                    
+                ''' Return x if close enough '''
+                
+                if(flag or alphas[-1] == 0):
+                    continue
+                
+               
+                else:
+    
+                    x = alphas[:(len(alphas)-1)]/alphas[-1]
+                    if(not(found_seed)):
+                        seed = x
+                        found_seed = True
+                    
+                    if(np.linalg.norm(np.matmul(self.A,x) - self.b) > max_error):
+                        if(iteration % 1000 == 0):
+                            print('Iteration=',iteration, 'error=', np.linalg.norm(np.matmul(self.A,x) - self.b))
+                        continue
+                    else:
+                        
+                        return x, iteration,seed, timeit.default_timer() - start
+
     def plots(self,algo):
         
         max_iteration_list=[]
@@ -144,22 +252,25 @@ class Algorithms():
         max_iteration_list_g,max_iteration_list_j,max_iteration_list_s=[],[],[]
         size_list_g,size_list_j,size_list_s=[],[],[]
         time_list_g,time_list_j,time_list_s=[],[],[]
-        spec_rad_list_g,spec_rad_list_j,spec_rad_list_s=[],[],[]              
-        for i in range(10,100,10):
+        spec_rad_list_g,spec_rad_list_j,spec_rad_list_s=[],[],[]  
+       # error_list_g,error_list_j,error_list_s=[],[],[]            
+        for i in range(5,16,5):
             x=Algorithms(i)
             if algo=='gauss':
                 x_,error_l,max_iteration,spec_rad,time = x.gauss(float('inf'),1E-15)
             elif algo=='jacobi':
                 x_,error_l,max_iteration,spec_rad,time = x.jacobi(float('inf'),1E-15)
             elif algo=='sor':
-                x_,error_l,max_iteration,spec_rad,time = x.SOR(float('inf'),1E-15,0)
+                x_,error_l,max_iteration,spec_rad,time = x.SOR(float('inf'),1E-15)
             elif algo=='all':
                 x_g,error_l_g,max_iteration_g,spec_rad_g,time_g = x.gauss(float('inf'),1E-15)
                 x_j,error_l_j,max_iteration_j,spec_rad_j,time_j = x.jacobi(float('inf'),1E-15)
-                x_s,error_l_s,max_iteration_s,spec_rad_s,time_s = x.SOR(float('inf'),1E-15,0)
+                x_s,error_l_s,max_iteration_s,spec_rad_s,time_s = x.SOR(float('inf'),1E-15)
                 size = size_list[:]
                 
-
+   #             error_list_g.append(error_l_g)
+    #            error_list_j.append(error_l_j)
+     #           error_list_s.append(error_l_s)
                 spec_rad_list_g.append(spec_rad_g)
                 size_list_g.append(i)
                 time_list_g.append(time_g)
@@ -258,7 +369,7 @@ class Algorithms():
     
     def rate_plots(self,d):
         x=Algorithms(d)
-        w=0.5
+        w=1.5
         x_g,error_l_g,max_iteration_g,spec_rad_g,time_g = x.gauss(float('inf'),1E-15)
         x_j,error_l_j,max_iteration_j,spec_rad_j,time_j = x.jacobi(float('inf'),1E-15)
         x_s,error_l_s,max_iteration_s,spec_rad_s,time_s = x.SOR(float('inf'),1E-15,w)
@@ -268,11 +379,15 @@ class Algorithms():
         ax.set_title('Error vs Iterations')
         m=min(len(error_l_g),len(error_l_j),len(error_l_s))
         x_axis_coord=[p for p in range(m)]
+        #plt.yscale('log')
+        ax.set_yscale('log')
         ax.plot(x_axis_coord, error_l_g[:m],label = 'Gauss')
         ax.plot(x_axis_coord, error_l_j[:m],label = 'Jacobi')
         ax.plot(x_axis_coord, error_l_s[:m],label = 'SOR ('+str(w)+')')    
         ax.legend()        
         ax.grid(True)
+        
+ 
         
 
                
@@ -291,7 +406,9 @@ if __name__=='__main__':
    # alg.plots("gauss")
    # alg.plots("jacobi")
    # alg.plots("sor")
-   # alg.plots("all")
+    alg.plots("all")
    # alg.SOR_plot(msize)
-    alg.rate_plots(10)
+    #alg.TriangleAlgo(max_error)
+    #alg.rate_plots(10)
+            
             
